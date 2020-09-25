1dff875fe1f5d699cec4e2db7630b99e4765bc91
pallavikyama <kyamap98@gmail.com>
2020-09-25 12:59:55 +0530

    Added:Single player starts game at position 0 is added

--- Play.java
+++ Play.java
@@ -2,7 +2,16 @@ package snakeAndLadder;
 
 public class Play {
 
+	// CONSTANTS
+	public static final int START = 0;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to THE SNAKE AND LADDER game.\n");
+
+		// VARIABLES
+		int position = START;
+
+		// INITIAL POSITION OF PLAYER
+		System.out.println("Player begins at position " + position + "\n");
 	}
 }
